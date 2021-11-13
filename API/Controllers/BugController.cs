00475fcb87d02e9241949e6f5e76f91d0ee39463
Kushpreet <viriak@myumanitoba.ca>
2021-11-12 19:56:45 -0600

    Api error handling

---
+++ API/Controllers/BugController.cs
@@ -0,0 +1,42 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BugController : ApiBaseController
+    {
+        private readonly DataContext _context;
+        public BugController(DataContext context){
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret(){
+            return (ActionResult<string>)"secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound(){
+            var thing = _context.Users.Find(-1);
+            if(thing == null) return NotFound();
+            else return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError(){
+            var thing = _context.Users.Find(-1);
+            var returnVal = thing.ToString();
+            return returnVal;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> getBadRequest(){
+            return BadRequest("Very bad request");
+        }
+        
+    }
+}
--- API/DataTransferObj/RegisterDTO.cs
+++ API/DataTransferObj/RegisterDTO.cs
@@ -9,8 +9,10 @@ namespace API.DataTransferObj
     public class RegisterDTO
     {
         [Required]
+        [StringLength(16, MinimumLength = 1)]
         public string username { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string password { get; set; }
     }
 }
